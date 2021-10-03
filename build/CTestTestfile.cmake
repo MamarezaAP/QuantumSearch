945a5a6651e6c04022662d7ba3e4ff53164a0cdd
MamarezaAP <mamarezaart@gmail.com>
2021-10-03 11:11:12 +0330

    Add project files.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mamar/source/repos/QuantumSearch
+# Build directory: C:/Users/mamar/source/repos/QuantumSearch/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
